822097ab37aef4cd463739124d09d446b4e909b4
nick.shutkov <nick.shutkov@declara.com>
2015-08-08 16:40:36 -0700

    updated login - UI changed  Please enter the commit message for your changes. Lines starting

--- src/test/java/com/setngo/auth/loginTest.java
+++ src/test/java/com/setngo/auth/loginTest.java
@@ -1,9 +1,10 @@
 package com.setngo.auth;
 
 import java.util.Locale;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
 import com.github.javafaker.Faker;
 import com.setngo.pages.RegistrationPage;
 public class loginTest extends baseUiTest {
@@ -14,8 +15,6 @@ public class loginTest extends baseUiTest {
 	@BeforeClass
 	void setUp() throws Exception{
 		registrationPage = new RegistrationPage(dr);
-		wait.until(ExpectedConditions.visibilityOf(registrationPage.getFirstBtn()));
-		registrationPage.getFirstBtn().click();
 	}
 	
 	@Test
